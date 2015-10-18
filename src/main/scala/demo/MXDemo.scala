12f23b9da0a461eb13d078f6a65b10638cafe392
KasonChan <kasonl.chan@gmail.com>
2015-10-18 09:06:16 -0500

    Rename MXDemo

--- src/main/scala/demo/MXDemo.scala
+++ src/main/scala/demo/MXDemo.scala
@@ -6,7 +6,7 @@ import helperClass.MX
 /**
  * Created by kasonchan on 10/9/15.
  */
-object Demo {
+object MXDemo {
 
   def main(args: Array[String]) {
     val inputs = List(MX(Some(1.5), Some(3)), MX(Some(4), Some(2.5)))

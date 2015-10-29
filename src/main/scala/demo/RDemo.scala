b0661ba19c68332bddce91dc404d1c9b609c519c
KasonChan <kasonl.chan@gmail.com>
2015-10-29 12:10:45 -0500

    Add Q2-Q4

--- src/main/scala/demo/RDemo.scala
+++ src/main/scala/demo/RDemo.scala
@@ -15,9 +15,9 @@ object RDemo {
                   tag: String,
                   spcode: String,
                   date: String,
-                  pdPSI: String,
-                  mdPSI: String,
-                  year: String)
+                  pdPSI: Option[Double],
+                  mdPSI: Option[Double],
+                  year: Int)
 
   def load(pathName: String): Either[String, Array[Array[String]]] = {
     Try {
@@ -38,7 +38,13 @@ object RDemo {
    */
   def transformToInfo(data: Array[Array[String]]): Array[Info] = {
     data.collect {
-      case Array(site, tag, spcode, date, pdPSI, mdPSI, year) => Info(site, tag, spcode, date, pdPSI, mdPSI, year)
+      case Array(site, tag, spcode, date, pdPSI, mdPSI, year) =>
+        (pdPSI, mdPSI) match {
+          case ("NA", "NA") => Info(site, tag, spcode, date, None, None, year.toInt)
+          case ("NA", m: String) => Info(site, tag, spcode, date, None, Some(m.toDouble), year.toInt)
+          case (p: String, "NA") => Info(site, tag, spcode, date, Some(p.toDouble), None, year.toInt)
+          case (p: String, m: String) => Info(site, tag, spcode, date, Some(p.toDouble), Some(m.toDouble), year.toInt)
+        }
     }
   }
 
@@ -65,6 +71,48 @@ object RDemo {
     }
   }
 
+  /**
+   * Mid day water potential should always be at least as negative as pre-dawn
+   * water potential. Are there any days and plants for which mid-day water
+   * potential is higher than pre-dawn?
+   * Returns the different species and dates
+   * @param i the array of info
+   */
+  def dates(i: Option[Array[Info]]): Option[Array[String]] = {
+    i match {
+      case None => None
+      case Some(a: Array[Info]) => Some(a.filter(x => x.mdPSI.getOrElse(0.0) > x.pdPSI.getOrElse(0.0))
+        .map(y => y.date).distinct)
+    }
+  }
+
+  /**
+   * What is the lowest (most negative) mid-day water potential in this data set?
+   * When and for which species was this value recorded?
+   * @param i the array of info
+   */
+  def lowestMdPSI(i: Option[Array[Info]]): Option[Array[(String, Option[Double], String)]] = {
+    i match {
+      case None => None
+      case Some(a: Array[Info]) =>
+        Some(a.filter(y =>
+          y.mdPSI.getOrElse(0.0) == a.map(x => x.mdPSI.getOrElse(0.0)).min)
+          .map(h => (h.spcode, h.mdPSI, h.date)))
+    }
+  }
+
+  /**
+   * For which year was the average mid day water potential lowest (most negative)?
+   * @param i the array of info
+   */
+  def lowestMdPSIAverageYear(i: Option[Array[Info]]) = {
+    i match {
+      case None => None
+      case Some(a: Array[Info]) => a.groupBy(_.year)
+        .map(i => (i._1, i._2.map(j => j.mdPSI.getOrElse(0.0)).min)).minBy(_._2)._1
+    }
+  }
+
   def main(args: Array[String]) {
     val data = load(pathName)
 
@@ -84,6 +132,26 @@ object RDemo {
     // 5 species
     val q1 = speciesCount(information).getOrElse(0)
     println(q1)
-  }
 
+    // Q2
+    // "07/21/11", "05/23/13", "05/24/13", "04/10/11", "05/22/13", "08/25/12"
+    val q2 = dates(information).getOrElse(Array(""))
+    println(q2.mkString("", ", ", ""))
+
+    // Q3
+    // The lowest mid-day water potential in this data set is -6.75, it is
+    // recorded on "04/10/11" for "QUGR3 " sp.
+    val q3 = lowestMdPSI(information).getOrElse(Array(("", None, "")))
+    q3.foreach(r =>
+      println("The lowest mid-day water potential in this data set is " +
+        r._2.getOrElse(0.0) +
+        ", it is recorded on " +
+        r._3 + " for " + r._1 + " sp."))
+
+    // Q4
+    // 11
+    val q4 = lowestMdPSIAverageYear(information)
+    println(q4)
+  }
+  
 }

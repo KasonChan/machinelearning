7fef31f1645b733579c59668bc450f7c6e8b4ea1
KasonChan <kasonl.chan@gmail.com>
2015-10-30 01:14:54 -0500

    Add Info, transformFlow, speciesFlow, and Q1

--- src/main/scala/demo/RADemo.scala
+++ src/main/scala/demo/RADemo.scala
@@ -4,6 +4,9 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Source}
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
 /**
  * Created by kasonchan on 10/19/15.
  */
@@ -15,15 +18,43 @@ object RADemo {
   private val CSV_DELIM = ","
   private val pathName = "resources/data/oak-water-potentials-simple.csv"
 
-  def load(pathName: String): Iterator[String] = io.Source.fromFile(pathName) getLines()
+  def load(pathName: String): Iterator[String] = io.Source.fromFile(pathName).getLines()
+
+  case class Info(site: String,
+                  tag: String,
+                  spcode: String,
+                  date: String,
+                  pdPSI: Option[Double],
+                  mdPSI: Option[Double],
+                  year: Int)
 
   def main(args: Array[String]) {
     val lines = Source(() => load(pathName))
-    val flow = Flow[String].map(line => line.split(",").toList)
+    val rowFlow = Flow[String].drop(1).map(line => line.split(",").toList)
 
-    (lines via flow).runForeach(println)
+    (lines via rowFlow).runForeach(println)
 
-    as awaitTermination()
+    val transformFlow = Flow[List[String]].collect {
+      case List(site, tag, spcode, date, pdPSI, mdPSI, year) =>
+        (pdPSI, mdPSI) match {
+          case ("NA", "NA") => Info(site, tag, spcode, date, None, None, year.toInt)
+          case ("NA", m: String) => Info(site, tag, spcode, date, None, Some(m.toDouble), year.toInt)
+          case (p: String, "NA") => Info(site, tag, spcode, date, Some(p.toDouble), None, year.toInt)
+          case (p: String, m: String) => Info(site, tag, spcode, date, Some(p.toDouble), Some(m.toDouble), year.toInt)
+        }
+    }
+
+    (lines via rowFlow via transformFlow).runForeach(println)
+
+    val speciesFlow = Flow[Info].groupBy(x => x.spcode)
+
+    /**
+     * Q1 How many different species are recorded in these data?
+     * 5 species
+     */
+    (lines via rowFlow via transformFlow via speciesFlow).runFold(0)((x, i) => x + 1).foreach(println)
+
+    as awaitTermination (60 seconds)
   }
 
 }

b2081fad958f41a6cb344e9f2f7590f53f607a70
KasonChan <kasonl.chan@gmail.com>
2015-10-30 16:54:28 -0500

    Add Q2-Q3, refactor code

--- src/main/scala/demo/RADemo.scala
+++ src/main/scala/demo/RADemo.scala
@@ -32,7 +32,11 @@ object RADemo {
     val lines = Source(() => load(pathName))
     val rowFlow = Flow[String].drop(1).map(line => line.split(",").toList)
 
-    (lines via rowFlow).runForeach(println)
+    val printlnSink = Sink.foreach(println)
+
+    //    Uncomment to print data
+    //    (lines via rowFlow).runForeach(println)
+    //    (lines via rowFlow).runWith(printlnSink)
 
     val transformFlow = Flow[List[String]].collect {
       case List(site, tag, spcode, date, pdPSI, mdPSI, year) =>
@@ -44,20 +48,56 @@ object RADemo {
         }
     }
 
-    (lines via rowFlow via transformFlow).runForeach(println)
-
-    val speciesFlow = Flow[Info].groupBy(x => x.spcode)
-    val speciesCountFlow = Flow[Info].groupBy(x => x.spcode).fold(0)((x, i) => x + 1)
-    val speciesCountSink = Sink.foreach(println)
+    //    Uncomment to print transformed data
+    //    (lines via rowFlow via transformFlow).runForeach(println)
+    //    (lines via rowFlow via transformFlow).runWith(printlnSink)
 
     /**
      * Q1 How many different species are recorded in these data?
      * 5 species
      */
-    (lines via rowFlow via transformFlow via speciesFlow).runFold(0)((x, i) => x + 1).foreach(println)
-    (lines via rowFlow via transformFlow via speciesCountFlow).runWith(speciesCountSink)
+    val speciesFlow = Flow[Info].groupBy(x => x.spcode)
+    val speciesCountFlow = Flow[Info].groupBy(x => x.spcode).fold(0)((x, i) => x + 1)
 
-    as awaitTermination (60 seconds)
+    (lines via rowFlow via transformFlow via speciesFlow)
+      .runFold(0)((x, i) => x + 1)
+      .foreach(println)
+
+    (lines via rowFlow via transformFlow via speciesCountFlow)
+      .runWith(printlnSink)
+
+    /**
+     * Q2 Mid day water potential should always be at least as negative as pre-dawn
+     * water potential. Are there any days and plants for which mid-day water
+     * potential is higher than pre-dawn?
+     * "08/25/12" "05/22/13" "04/10/11" "05/24/13" "05/23/13" "07/21/11"
+     */
+    val dateFlow = Flow[Info].filter(x =>
+      x.mdPSI.getOrElse(0.0) > x.pdPSI.getOrElse(0.0))
+      .groupBy(y => y.date)
+      .map(z => z._1)
+
+    (lines via rowFlow via transformFlow via dateFlow)
+      .runFold("")((x, i) => i + " " + x).foreach(println)
+
+    /**
+     * Q3 What is the lowest (most negative) mid-day water potential in this data set?
+     * When and for which species was this value recorded?
+     */
+    val lowestMdPSIFlow = Flow[Info].fold(Info("", "", "", "", None, None, 0))((m, i) =>
+      if (i.mdPSI.getOrElse(0.0) < m.mdPSI.getOrElse(0.0)) i else m).map(x => (x.spcode, x.mdPSI, x.date))
+
+    (lines via rowFlow via transformFlow via lowestMdPSIFlow)
+      .runForeach(r =>
+        println("The lowest mid-day water potential in this data set is " +
+          r._2.getOrElse(0.0) +
+          ", it is recorded on " +
+          r._3 +
+          " for " +
+          r._1 +
+          " sp."))
+
+    as awaitTermination (2 minutes)
   }
 
 }
